1df86816ba9e6bd538213ece20afed734e7d3bf8
meikakuzen <meikakuzen@outlook.com>
2024-03-06 19:14:27 +0100

    RN Adapter http

--- REACT_NATIVE/04-Pelis/src/config/adapters/http/axios.adapter.tsx
+++ REACT_NATIVE/04-Pelis/src/config/adapters/http/axios.adapter.tsx
@@ -1,12 +1,27 @@
 import { HttpAdapter } from "./http.adapter";
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
+
+interface Options{
+    baseURL: string,
+    params: Record<string,string>
+}
 
 export class AxiosAdapter implements HttpAdapter{
+
+    private axiosInstance : AxiosInstance;
+
+    constructor(options: Options){
+        this.axiosInstance = axios.create({
+            baseURL: options.baseURL,
+            params: options.params
+        })
+    }
+
 
     async get<T>(url: string, options?: Record<string, unknown>) : Promise<T>{
 
         try {
-            const {data} = await axios.get<T>(url,options)
+            const {data} = await this.axiosInstance.get<T>(url,options)
             
             return data
         
